27c0d8aa2f1aef6ad61c4254387429497cc78536
Robin Haider <robinhaider69@gmail.com>
2019-02-23 01:58:11 +0600

    Gallery section complete

--- Project 4 - Bookstore Website/js/script.js
+++ Project 4 - Bookstore Website/js/script.js
@@ -5,5 +5,17 @@ $(document).ready(function () {
         $('.navigation').toggleClass('change');
     });
 
+    lightbox.option({
+        'wrapAround': true
+    });
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if(position>=350) {
+            $('.gallery').addClass('change');
+        } else {
+            $('.gallery').removeClass('change');
+        }
+    });
 
 });
